c21d881e40be8623405c771369b89e97cdf3ca27
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-09-25 01:01:28 +0900

    routing

--- Binary_server/src/router/Breserve/index.js
+++ Binary_server/src/router/Breserve/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+const Breserve = require('../../model/Breserve');
 const BreserveCtrl = require('./Breseve.ctrl');
 
 router.post('/', BreserveCtrl.PostBreserve);
+router.get('/', BreserveCtrl.GetBreserve);
+
+router.get('/:idx', BreserveCtrl.GetBreserves);
 
 module.exports = router;
