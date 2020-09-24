ad691daa266cfece52d2297363c96359543c93a7
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-09-25 01:02:49 +0900

    del one line in Breserve/index

--- Binary_server/src/router/Breserve/index.js
+++ Binary_server/src/router/Breserve/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const Breserve = require('../../model/Breserve');
 const BreserveCtrl = require('./Breseve.ctrl');
 
 router.post('/', BreserveCtrl.PostBreserve);
